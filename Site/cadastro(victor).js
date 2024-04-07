c210b16b80331585765f2636216070a7cc12a2dc
Vitinn04 <victor.batista@sptech.school>
2024-04-07 17:06:17 -0300

    Tela cadastro V.2

---
+++ Site/cadastro(victor).js
@@ -0,0 +1,24 @@
+var nomeEmpresa = input_empresa.value;
+var cnpj = input_cnpj.value;
+var emailEmpresa = input_emailEmpresa.value;
+var telefone = input_telefone.value;
+var nomeGestor = input_gestor.value;
+var emailGestor = input_emailGestor.value;
+var senha = input_senha.value;
+var confirmarSenha = input_confirmarSenha.value;
+
+function validarcnpj(){
+    
+}
+function validarEmailEmpresa() {
+
+}
+function validarTelefone(){
+
+}
+function validarEmailGestor(){
+
+}
+function validarSenha() {
+
+}
