ee3f8dc98da17c97030ed02026530c236af8f75f
moisesaraujo2005 <moises.silva@sptech.school>
2024-04-10 19:49:11 -0300

    Atualizando site css e html, uso de vh, vw e rem, em vez de px

--- Site/animação.js
+++ Site/animação.js
@@ -1,10 +1,15 @@
+
+/*Função IntersectionObserver - Está observando todos os elementos da página, desde div a imagens. */
 var ObservandoElementos = new IntersectionObserver((entrada) => {
+
 
     entrada.forEach((entre) => {
         console.log(entrada)
+        /* Se o intersecting for true, irá aparecer no inspecionar, e fazendo uma decisão, se ele for true, crie uma div "show" com o opacity 1*/
         if(entre.isIntersecting === true) {
             entre.target.classList.add('show')
         }
+        /*Se o intersecting for false, remova a tag show*/
         else {
             entre.target.classList.remove('show')
         }
@@ -12,7 +17,9 @@ var ObservandoElementos = new IntersectionObserver((entrada) => {
     }) 
 })
 
+/* Está pegando a class invisivel que deixa os elementos em opacity 0 no caso "invisivel" */
 var elementosInvisiveis = document.querySelectorAll('.invisivel')
 
+/* Observando cada elemento da tela "ForEach = Para Cada", para cada elemento ele observara*/
 elementosInvisiveis.forEach((elemento) => ObservandoElementos.observe(elemento))
 
