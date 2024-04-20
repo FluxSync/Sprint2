1a3d41e69aa3fbbb3aa0b55ac2154ea7cad0b59d
moisesaraujo2005 <moises.silva@sptech.school>
2024-04-20 18:11:54 -0300

    Atualização final dos comentários na animação

--- Site/animação.js
+++ Site/animação.js
@@ -1,8 +1,9 @@
 
-/*Função IntersectionObserver - Está observando todos os elementos da página, desde div a imagens. */
+/*Função IntersectionObserver - Está observando todos os elementos da página, desde div a imagens e cria uma lista com os elementos chamada
+entrada. */
 var ObservandoElementos = new IntersectionObserver((entrada) => {
 
-
+/*Entre representa um elemento dentro da lista entrada*/
     entrada.forEach((entre) => {
         console.log(entrada)
         /* Se o intersecting for true, irá aparecer no inspecionar, e fazendo uma decisão, se ele for true, crie uma div "show" com o opacity 1*/
@@ -20,6 +21,8 @@ var ObservandoElementos = new IntersectionObserver((entrada) => {
 /* Está pegando a class invisivel que deixa os elementos em opacity 0 no caso "invisivel" */
 var elementosInvisiveis = document.querySelectorAll('.invisivel')
 
-/* Observando cada elemento da tela "ForEach = Para Cada", para cada elemento ele observara*/
+/* Observando cada elemento da tela "ForEach = Para Cada", para cada elemento ele observara,
+ método observe é especifico da classe interSectionObject*/
+ 
 elementosInvisiveis.forEach((elemento) => ObservandoElementos.observe(elemento))
 
